34668bdf1ea0d42e78fe8aa3ef5e38f1b022bc95
All_man <zousaaan3@gmail.com>
2019-11-18 02:29:08 +0900

    Finish 4.2 (fix DiceRoll for Homework)

--- diseRoller/app/src/main/java/com/all_man/diseroller/MainActivity.kt
+++ diseRoller/app/src/main/java/com/all_man/diseroller/MainActivity.kt
@@ -2,14 +2,21 @@ package com.all_man.diseroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.ImageView
 import java.util.*
 
+
+const val KEY_DICENUMRESOURCE = "diceNumResource_key"
+const val KEY_DICENUMRESOURCE2 = "diceNumResource2_key"
 class MainActivity : AppCompatActivity() {
 
     lateinit var diceImage: ImageView
     lateinit var diceImage2: ImageView
+
+    private var diceNumResource = 0
+    private var diceNumResource2 = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,16 +25,25 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         diceImage = findViewById(R.id.dice_image)
         diceImage2 = findViewById(R.id.dice_image2)
+
+        if (savedInstanceState != null) {
+            diceNumResource = savedInstanceState.getInt("diceNumResource_key", 0)
+            diceNumResource2 = savedInstanceState.getInt("diceNumResource2_key", 0)
+
+            diceImage.setImageResource(diceNumResource)
+            diceImage2.setImageResource(diceNumResource2)
+        }
         rollButton.setOnClickListener { rollDice() }
     }
 
     private fun rollDice() {
-        diceImage.setImageResource(setImageResource())
-        diceImage2.setImageResource(setImageResource())
+        diceNumResource = setImageResource()
+        diceNumResource2 = setImageResource()
+        diceImage.setImageResource(diceNumResource)
+        diceImage2.setImageResource(diceNumResource2)
     }
 
     private fun setImageResource(): Int {
-
         val randomInt = Random().nextInt(6) + 1
         val imageResource = when (randomInt) {
             1 -> R.drawable.dice_1
@@ -39,5 +55,12 @@ class MainActivity : AppCompatActivity() {
         }
 
         return imageResource
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putInt(KEY_DICENUMRESOURCE, diceNumResource)
+        outState.putInt(KEY_DICENUMRESOURCE2, diceNumResource2)
     }
 }
